6e24f1d8a2304fba0adc67c62aa8680ecfd9e5b9
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-06-30 05:03:39 +0400

    Update max(data).py

--- max(data).py
+++ max(data).py
@@ -1,12 +1,13 @@
-def max(data):
-    '''max(data) return the maximum value of the data'''
+def max_(data):
+    # max_(data) return the maximum value of the data
     i = 1
-    max = data[0]
+    max1 = data[0]
     while i < len(data):
-        if data[i] > max:
-            max = data[i]
-        i +=1
-    return max
+        if data[i] > max1:
+            max1 = data[i]
+        i += 1
+    return max1
 
-data = [1,7,8,-1]
-print(max(data))
+
+data1 = [1, 7, 8, -1]
+print(max_(data1))
