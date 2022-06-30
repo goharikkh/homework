8b48d1b482455204232f9f641a990a5aa7c45c03
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-06-30 05:05:11 +0400

    Update min(data).py

--- min(data).py
+++ min(data).py
@@ -1,12 +1,13 @@
-def min(data):
-    '''min(data) return minimum value of the data'''
+def min_(data):
+    # min(data) return minimum value of the data
     i = 1
-    min = data[0]
+    min1 = data[0]
     while i < len(data):
-        if data[i] < min:
-            min = data[i]
-        i +=1
-    return min
+        if data[i] < min1:
+            min1 = data[i]
+        i += 1
+    return min1
 
-data = [1,7,8,-1]
-print(min(data))
+
+data1 = [1, 7, 8, -1]
+print(min(data1))
