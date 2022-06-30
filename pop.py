3e7aee38f3f11f78f6e845be5925833691497a86
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-06-30 04:51:13 +0400

    Update pop.py

--- pop.py
+++ pop.py
@@ -1,11 +1,11 @@
 def pop(i, data):
-    '''pop(i, date) returns data without last element if i = None else returns i-rd element of data'''
-    if i == None:
+    # pop(i, date) returns data without last element if i = None else returns i-rd element of data
+    if i is None:
         n = len(data) - 1
         data.remove(data[n])
         return data
-    else :
-        return data[i]
+    return data[i]
 
-data=[1,5,7,8]
-print(pop(None,data))
+
+data1 = [1, 5, 7, 8]
+print(pop(None, data1))
