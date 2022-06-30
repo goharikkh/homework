e5257a694b7fb3124f657c239013f0855dd1097d
goharikkh <100861779+goharikkh@users.noreply.github.com>
2022-06-30 05:01:06 +0400

    Update max_pow.py

--- max_pow.py
+++ max_pow.py
@@ -1,10 +1,11 @@
-def max_pow(x,y,z):
-    '''max_pow(x,y,z) returns the sum of the 2 biggest numbers square '''
+def max_pow(x, y, z):
+    # max_pow(x,y,z) returns the sum of the 2 biggest numbers square
     if x < y < z:
-        return y**2 + z**2
+        return y ** 2 + z ** 2
     elif y < x < z:
-        return x**2 + z**2
+        return x ** 2 + z ** 2
     else:
-        return x**2 + y**2
+        return x ** 2 + y ** 2
 
-print(max_pow(2,3,4))
+
+print(max_pow(2, 3, 4))
